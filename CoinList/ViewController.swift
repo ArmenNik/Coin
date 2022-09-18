b65057e8e990c7ca051b5b1fbd3931380cd4cba1
Armen Nikoghosyan <armnikoghosyan@ghost-services.com>
2022-09-18 12:52:16 +0400

    Initial Commit

---
+++ CoinList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinList
+//
+//  Created by Armen Nikoghosyan on 18/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
